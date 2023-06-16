ba9bbcf300fd7b5b3c9c360b988354f30aafb0df
Noodle <yokaii.noodle@gmail.com>
2023-06-17 00:03:54 +0300

    NPC, Healthbar, NPCSpawner, URP
    NPC eklendi.
    Can barı eklendi.
    NPC Spawner eklendi.
    Universal RP eklendi. URP ayarları düzenlendi.
    Zemine Rigidbody eklendi.
    AI Navigation eklendi.

---
+++ Assets/Scripts/NPCSpawner.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+
+public class NPCSpawner : MonoBehaviour
+{
+    public GameObject npcPrefab;
+    public float spawnInterval = 3f;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnNPC());
+    }
+
+    IEnumerator SpawnNPC()
+    {
+        while (true)
+        {
+            Instantiate(npcPrefab, transform.position, transform.rotation);
+            yield return new WaitForSeconds(spawnInterval);
+        }
+    }
+}
