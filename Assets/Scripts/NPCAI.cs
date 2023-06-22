3f5e9a519f30d9da3097f81cfc3ccce23a0be50d
Noodle <yokaii.noodle@gmail.com>
2023-06-22 23:32:55 +0300

    NPC Takip ve Nav Mesh
    NPC Takip ve Nav Mesh eklendi.

---
+++ Assets/Scripts/NPCAI.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCAI : MonoBehaviour
+{
+    public NavMeshAgent _agent;
+    [SerializeField] Transform _player;
+    public LayerMask ground, player;
+
+    private void Awake()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        ChasePlayer();
+    }
+
+    void ChasePlayer()
+    {
+        if (_player != null)
+        {
+            _agent.SetDestination(_player.position);
+        }
+    }
+}
