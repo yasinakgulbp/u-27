032328d193ba7a9fa1b01ab9be29c65d50cd25d2
Yasin Akgül <62186511+yasinakgulbp@users.noreply.github.com>
2023-06-26 05:54:39 +0300

    Enemy ölüğünde İhtimalli coin ödül sistemi eklendi
    Her enemy öldüğünde:
     %50 ihtimalle 1 puna veren coin
     %30 ihtimalle 5 puna veren coin
     %20 ihtimalle 15 puna veren coin
    düşüyor.

--- Assets/Scripts/enemy6.cs
+++ Assets/Scripts/enemy6.cs
@@ -6,8 +6,8 @@ public class enemy6 : MonoBehaviour
 {
     public int Health = 10;
     public GameObject coinPrefab;
-    //public GameObject coinPrefab2;
-    //public GameObject coinPrefab3;
+    public GameObject coinPrefab2;
+    public GameObject coinPrefab3;
 
     public void Damage(int damage)
     {
@@ -21,7 +21,27 @@ public class enemy6 : MonoBehaviour
 
     private void SpawnCoin()
     {
-        GameObject coin = Instantiate(Resources.Load<GameObject>("Coin"), transform.position, Quaternion.identity);
-        coin.SetActive(true);
+        GameObject coin = null;
+
+        float randomValue = Random.value; // Rastgele bir deðer alýnýr (0 ile 1 arasýnda)
+
+        if (randomValue < 0.5f) // %50 olasýlýk
+        {
+            coin = Instantiate(coinPrefab, transform.position, Quaternion.identity);
+        }
+        else if (randomValue < 0.8f) // %30 olasýlýk
+        {
+            coin = Instantiate(coinPrefab2, transform.position, Quaternion.identity);
+        }
+        else // %20 olasýlýk
+        {
+            coin = Instantiate(coinPrefab3, transform.position, Quaternion.identity);
+        }
+
+        if (coin != null)
+        {
+            coin.SetActive(true);
+        }
     }
+
 }
