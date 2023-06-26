e54c0976ae7be13e0087be6a04ac21e20a3270bb
Noodle <yokaii.noodle@gmail.com>
2023-06-26 23:42:45 +0300

    Pause Menu added.
    Pause Menu added.
    Devam et, yeniden başla, çıkış butonları eklendi.
    PauseMenu adındaki script eklendi.
    Yeniden başla butonunda sadece Coin üzerine yazılarak devam ediyor. Bu ayar düzeltilmeli.

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+
+    private bool isPaused = true;
+
+    void Start()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+
+        // Mouse imleci gösterilsin
+        Cursor.visible = true;
+        // Mouse yakalamasý kapatýlsýn
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                Resume();
+            else
+                Pause();
+        }
+
+        // Mouse imleci kaybolmamasýný saðlayan kod
+         if (isPaused)
+         {
+             Cursor.visible = true;
+             Cursor.lockState = CursorLockMode.Confined;
+         }
+         else
+         {
+             Cursor.visible = false;
+             Cursor.lockState = CursorLockMode.Locked;
+         }
+
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void Restart()
+    {
+        // Sahneyi yeniden yükle
+        Scene currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.buildIndex);
+
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void Quit()
+    {
+        UnityEditor.EditorApplication.isPlaying = false; //Bu kodu Build alacaðýmýz zaman sileceðiz. Yoksa çalýþmýyor. Fakat, þimdilik test ederken görebilmemiz için durmasý gerekiyor.
+        Application.Quit();
+    }
+
+    private void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+}
