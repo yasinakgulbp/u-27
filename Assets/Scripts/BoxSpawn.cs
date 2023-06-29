938349c54dbaee49555ab6947f6c2fee72fd61c1
Yasin Akgül <62186511+yasinakgulbp@users.noreply.github.com>
2023-06-29 16:37:45 +0300

    Box ve Box Spawnerlar yapıldı (Odül sistemi)
    Artık oyun başladığında oyun haritasının random belirlenen yerlerine belirlenen sayıda ödül sandığı düşüyor
    Sahneye: BoxSpawner eklendi.
    Scriptlere: BoxSpawn Scripti eklendi

---
+++ Assets/Scripts/BoxSpawn.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class BoxSpawn : MonoBehaviour
+{
+    public GameObject boxPrefab; // Ödül sandýðý prefabý
+    public int boxCount; // Spawn edilecek sandýk sayýsý
+    public float mapWidth; // Haritanýn geniþliði
+    public float mapLength; // Haritanýn uzunluðu
+
+    private void Start()
+    {
+        // Belirtilen sayýda sandýk spawn et
+        for (int i = 0; i < boxCount; i++)
+        {
+            // Rastgele bir konum seç
+            float spawnX = Random.Range(-mapWidth / 2f, mapWidth / 2f);
+            float spawnZ = Random.Range(-mapLength / 2f, mapLength / 2f);
+            Vector3 spawnPos = new Vector3(spawnX, 0f, spawnZ);
+
+            // Sandýðý spawn et
+            Instantiate(boxPrefab, spawnPos, Quaternion.identity);
+        }
+    }
+}
