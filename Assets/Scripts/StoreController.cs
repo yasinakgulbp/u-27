f6e90bc9d9facf6c1d2937a9874467ebed2b4faf
Noodle <yokaii.noodle@gmail.com>
2023-07-12 21:03:53 +0300

    Shopping System
    Store'da tıklayınca, mevcut "money" değerimizden gidiyor.
    100, 200, 300, 400, 500 money harcayabiliyoruz.
    İlk satırdaki butonlara eklendiler.

---
+++ Assets/Scripts/StoreController.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class StoreController : MonoBehaviour
+{
+    public TMP_Text MoneyCount;
+    private int money;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SatýnAlim1()
+    {
+        money = PlayerPrefs.GetInt("money");
+        money = money - 100;
+        PlayerPrefs.SetInt("money", money);
+        MoneyCount.text = PlayerPrefs.GetInt("money").ToString();
+    }
+
+    public void SatýnAlim2()
+    {
+        money = PlayerPrefs.GetInt("money");
+        money = money - 200;
+        PlayerPrefs.SetInt("money", money);
+        MoneyCount.text = PlayerPrefs.GetInt("money").ToString();
+    }
+
+    public void SatýnAlim3()
+    {
+        money = PlayerPrefs.GetInt("money");
+        money = money - 300;
+        PlayerPrefs.SetInt("money", money);
+        MoneyCount.text = PlayerPrefs.GetInt("money").ToString();
+    }
+
+    public void SatýnAlim4()
+    {
+        money = PlayerPrefs.GetInt("money");
+        money = money - 400;
+        PlayerPrefs.SetInt("money", money);
+        MoneyCount.text = PlayerPrefs.GetInt("money").ToString();
+    }
+
+    public void SatýnAlim5()
+    {
+        money = PlayerPrefs.GetInt("money");
+        money = money - 500;
+        PlayerPrefs.SetInt("money", money);
+        MoneyCount.text = PlayerPrefs.GetInt("money").ToString();
+    }
+}
