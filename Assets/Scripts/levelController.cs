71c9b294efbab578d7488360f19ecc24623a5151
Noodle <yokaii.noodle@gmail.com>
2023-07-14 01:37:19 +0300

    Button sounds added.
    Button sounds added.

--- Assets/Scripts/levelController.cs
+++ Assets/Scripts/levelController.cs
@@ -13,10 +13,11 @@ public class levelController : MonoBehaviour
     public TMP_Text MoneyCountText2;
 
     public GameObject complated;
-
     public GameObject StoreUI;
-
     public GameObject LevelUI;
+
+    public AudioClip buttonSound;
+    private AudioSource audioSource;
 
 
     private void Awake()
@@ -28,18 +29,22 @@ public class levelController : MonoBehaviour
     {
         SceneManager.LoadScene(_sahne.buildIndex + 1);
         //PlayerPrefs.SetInt("ScoreCounts", score.instance.collectedPoint);
+        ButtonSound();
     }
 
     public void ExitGame()
     {
         UnityEditor.EditorApplication.isPlaying = false; //Bu kodu Build alacaðýmýz zaman sileceðiz. Yoksa çalýþmýyor. Fakat, þimdilik test ederken görebilmemiz için durmasý gerekiyor.
         Application.Quit();
+        ButtonSound();
     }
 
     public void Start()
     {
         MoneyCountText.text = PlayerPrefs.GetInt("money").ToString();
         MoneyCountText2.text = PlayerPrefs.GetInt("money").ToString();
+
+        audioSource = GetComponent<AudioSource>();
 
         //PlayerPrefs.DeleteKey("levelend");  //Süremizi sýfýrlamak için burayý ekledim.
         if (PlayerPrefs.GetInt("levelend") == 1)
@@ -54,12 +59,14 @@ public class levelController : MonoBehaviour
     public void StoreButton()
     {
         StoreUI.SetActive(true);
+        ButtonSound();
 
     }
 
     public void LevelButton()
     {
         LevelUI.SetActive(true);
+        ButtonSound();
 
     }
 
@@ -67,5 +74,12 @@ public class levelController : MonoBehaviour
     {
         LevelUI.SetActive(false);
         StoreUI.SetActive(false);
+        ButtonSound();
+    }
+
+    public void ButtonSound()
+    {
+        audioSource.clip = buttonSound; // Ses dosyasýný atama
+        audioSource.Play(); // Ses dosyasýný çal
     }
 }
